8b5660c8af88a3e675339ea4bbf501d56a61d792
ace <ace@acedeMacBook-Pro.local>
2019-11-25 16:34:14 +0800

    project init

--- src/main.js
+++ src/main.js
@@ -4,7 +4,13 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+// 移动端
+import fastClick from 'fastclick'
+import './assets/styles/reset.css'
+import './assets/styles/border.css'
+
 Vue.config.productionTip = false
+fastClick.attach(document.body)
 
 /* eslint-disable no-new */
 new Vue({
---
+++ src/pages/home/Home.vue
@@ -0,0 +1,17 @@
+<template>
+	<div>	
+		<div class="home">Home world </div>
+		<!-- <router-link to="/list">列表页</router-link> -->
+	</div>
+</template> 
+
+<script>
+	export default {
+		name: 'Home'
+	}
+</script>
+<style>	
+.home {
+	font-size: 50px;
+}
+</style>
